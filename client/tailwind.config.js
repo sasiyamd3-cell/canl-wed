445ccdaad0e036c3ae3dc2407589f30820f5436d
sasiyamd3-cell <sasiyamd3@gmail.com>
2026-09-20 00:15:38 -0700

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0a0a0f',
+        'card-bg': '#14141f',
+        'neon-purple': '#8b5cf6',
+        'neon-pink': '#ec4899',
+      },
+    },
+  },
+  plugins: [],
+}
